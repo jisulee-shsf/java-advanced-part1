b7ebf0ffbba08e7e9082c0bf579f5965a851321e
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-08 13:22:35 +0900

    test: [스레드 제어와 생명 주기1] 체크 예외 재정의

---
+++ src/thread/control/CheckedExceptionMain.java
@@ -0,0 +1,18 @@
+package thread.control;
+
+import util.ThreadUtils;
+
+public class CheckedExceptionMain {
+
+    public static void main(String[] args) throws Exception {
+        throw new Exception();
+    }
+
+    static class CheckedRunnable implements Runnable {
+        @Override
+        public void run()  /*throws Exception*/ {
+//            throw new Exception();
+            ThreadUtils.sleep(1000);
+        }
+    }
+}
