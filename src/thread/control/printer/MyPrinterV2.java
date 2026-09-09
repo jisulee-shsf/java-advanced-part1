07da8e978132f5bfa5456281be61f86f4ce9db18
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-09 12:34:50 +0900

    refactor: [스레드 제어와 생명 주기2] 프린터 예제 - 인터럽트 코드 개선

--- src/thread/control/printer/MyPrinterV2.java
+++ src/thread/control/printer/MyPrinterV2.java
@@ -1,4 +1,4 @@
-package thread.control.print;
+package thread.control.printer;
 
 import java.util.Queue;
 import java.util.Scanner;
--- src/thread/control/printer/MyPrinterV3.java
+++ src/thread/control/printer/MyPrinterV3.java
@@ -1,13 +1,12 @@
-package thread.control.print;
+package thread.control.printer;
 
 import java.util.Queue;
 import java.util.Scanner;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static util.MyLogger.log;
-import static util.ThreadUtils.sleep;
 
-public class MyPrinterV1 {
+public class MyPrinterV3 {
 
     public static void main(String[] args) {
         Printer printer = new Printer();
@@ -19,7 +18,7 @@ public class MyPrinterV1 {
             log("출력할 문서를 입력하세요. 종료 (q) = ");
             String input = scanner.nextLine();
             if (input.equals("q")) {
-                printer.work = false;
+                printerThread.interrupt();
                 break;
             }
             printer.addJob(input);
@@ -27,19 +26,23 @@ public class MyPrinterV1 {
     }
 
     static class Printer implements Runnable {
-        volatile boolean work = true;
         Queue<String> jobQueue = new ConcurrentLinkedQueue<>();
 
         @Override
         public void run() {
-            while (work) {
+            while (!Thread.interrupted()) {
                 if (jobQueue.isEmpty()) {
                     continue;
                 }
-                String job = jobQueue.poll();
-                log("출력 시작 = " + job + " / 대기 문서 = " + jobQueue);
-                sleep(3000);
-                log("출력 완료 = " + job);
+                try {
+                    String job = jobQueue.poll();
+                    log("출력 시작 = " + job + " / 대기 문서 = " + jobQueue);
+                    Thread.sleep(3000);
+                    log("출력 완료 = " + job);
+                } catch (InterruptedException e) {
+                    log("InterruptedException 발생");
+                    break;
+                }
             }
             log("프린터 종료");
         }
