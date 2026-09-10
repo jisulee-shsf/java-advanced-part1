633ec026b3a4cd66d92b63f076dbd5c1be2af1dd
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-10 15:36:28 +0900

    refactor: [고급 동기화] 출금 예제 - ReentrantLock 사용

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -8,7 +8,8 @@ public class BankMain {
     public static void main(String[] args) throws InterruptedException {
 //        BankAccount account = new BankAccountV1(1000);
 //        BankAccount account = new BankAccountV2(1000);
-        BankAccount account = new BankAccountV3(1000);
+//        BankAccount account = new BankAccountV3(1000);
+        BankAccount account = new BankAccountV4(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
@@ -24,5 +25,18 @@ public class BankMain {
         t2.join();
 
         log("최종 잔액 = " + account.getBalance());
+        /*
+        15:20:38.195 [       t1] 거래 시작 = BankAccountV4
+        15:20:38.195 [       t2] 거래 시작 = BankAccountV4
+        15:20:38.218 [       t1] [검증 시작] 출금액 - 800, 잔액 - 1000
+        15:20:38.218 [       t1] [검증 완료] 출금액 - 800, 잔액 - 1000
+        15:20:38.620 [     main] t1 상태 = TIMED_WAITING
+        15:20:38.620 [     main] t2 상태 = WAITING -> BLOCKED X
+        15:20:39.223 [       t1] [출금 완료] 출금액 - 800, 잔액 - 200
+        15:20:39.223 [       t1] 거래 종료
+        15:20:39.223 [       t2] [검증 시작] 출금액 - 800, 잔액 - 200
+        15:20:39.225 [       t2] [검증 실패] 출금액 - 800, 잔액 - 200
+        15:20:39.230 [     main] 최종 잔액 = 200
+        */
     }
 }
