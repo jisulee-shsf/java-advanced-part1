38c3e9861b0d5a15455e9bcb27b9be5affee6382
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-10 16:14:11 +0900

    refactor: [고급 동기화] 출금 예제 - ReentrantLock 대기 중단

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -9,7 +9,9 @@ public class BankMain {
 //        BankAccount account = new BankAccountV1(1000);
 //        BankAccount account = new BankAccountV2(1000);
 //        BankAccount account = new BankAccountV3(1000);
-        BankAccount account = new BankAccountV4(1000);
+//        BankAccount account = new BankAccountV4(1000);
+//        BankAccount account = new BankAccountV5(1000);
+        BankAccount account = new BankAccountV6(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
@@ -26,17 +28,16 @@ public class BankMain {
 
         log("최종 잔액 = " + account.getBalance());
         /*
-        15:20:38.195 [       t1] 거래 시작 = BankAccountV4
-        15:20:38.195 [       t2] 거래 시작 = BankAccountV4
-        15:20:38.218 [       t1] [검증 시작] 출금액 - 800, 잔액 - 1000
-        15:20:38.218 [       t1] [검증 완료] 출금액 - 800, 잔액 - 1000
-        15:20:38.620 [     main] t1 상태 = TIMED_WAITING
-        15:20:38.620 [     main] t2 상태 = WAITING -> BLOCKED X
-        15:20:39.223 [       t1] [출금 완료] 출금액 - 800, 잔액 - 200
-        15:20:39.223 [       t1] 거래 종료
-        15:20:39.223 [       t2] [검증 시작] 출금액 - 800, 잔액 - 200
-        15:20:39.225 [       t2] [검증 실패] 출금액 - 800, 잔액 - 200
-        15:20:39.230 [     main] 최종 잔액 = 200
+        16:09:06.924 [       t1] 거래 시작 = BankAccountV6
+        16:09:06.924 [       t2] 거래 시작 = BankAccountV6
+        16:09:06.955 [       t1] [검증 시작] 출금액 - 800, 잔액 - 1000
+        16:09:06.957 [       t1] [검증 완료] 출금액 - 800, 잔액 - 1000
+        16:09:07.356 [     main] t1 상태 = TIMED_WAITING -> sleep(1000)
+        16:09:07.358 [     main] t2 상태 = TIMED_WAITING -> tryLock(500)
+        16:09:07.442 [       t2] [진입 실패] 이미 처리중인 작업이 있습니다.
+        16:09:07.963 [       t1] [출금 완료] 출금액 - 800, 잔액 - 200
+        16:09:07.963 [       t1] 거래 종료
+        16:09:07.969 [     main] 최종 잔액 = 200
         */
     }
 }
