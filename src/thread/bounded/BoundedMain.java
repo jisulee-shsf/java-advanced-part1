1e7e3435a3289a044342d307078041fdfe83319b
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-12 04:09:32 +0900

    refactor: [생산자 소비자 문제] 반복문 사용

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -9,7 +9,8 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
 
     public static void main(String[] args) {
-        BoundedQueue queue = new BoundedQueueV1(2);
+//        BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
         producerFirst(queue);
         consumerFirst(queue);
