1e490eb7ba349816209fdb44618511d5082280bb
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-12 17:41:46 +0900

    test: [생산자 소비자 문제] BlockingQueue 기능

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -14,10 +14,13 @@ public class BoundedMain {
 //        BoundedQueue queue = new BoundedQueueV3(2);
 //        BoundedQueue queue = new BoundedQueueV4(2);
 //        BoundedQueue queue = new BoundedQueueV5(2);
-        BoundedQueue queue = new BoundedQueueV6_1(2);
+//        BoundedQueue queue = new BoundedQueueV6_1(2);
+//        BoundedQueue queue = new BoundedQueueV6_2(2);
+//        BoundedQueue queue = new BoundedQueueV6_3(2);
+        BoundedQueue queue = new BoundedQueueV6_4(2);
 
-//        producerFirst(queue);
-        consumerFirst(queue);
+        producerFirst(queue);
+//        consumerFirst(queue);
     }
 
     private static void producerFirst(BoundedQueue queue) {
