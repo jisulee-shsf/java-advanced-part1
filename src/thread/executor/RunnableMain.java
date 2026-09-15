f954ca707ef419a603973d06ae122f0f6c40a1ec
jisulee-shsf <jisulee.shsf@gmail.com>
2026-09-15 23:30:40 +0900

    test: [스레드 풀과 Executor 프레임워크] Runnable의 불편함

---
+++ src/thread/executor/RunnableMain.java
@@ -0,0 +1,37 @@
+package thread.executor;
+
+import java.util.Random;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class RunnableMain {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyRunnable task = new MyRunnable();
+        Thread thread = new Thread(task, "thread-1");
+        thread.start();
+        thread.join();
+        int result = task.value;
+        log("result = " + result);
+        /*
+        23:29:20.774 [  thread1] Runnable 시작
+        23:29:22.794 [  thread1] value = 2
+        23:29:22.794 [  thread1] Runnable 종료
+        23:29:22.795 [     main] result = 2
+        */
+    }
+
+    static class MyRunnable implements Runnable {
+        private int value;
+
+        @Override
+        public void run() {
+            log("Runnable 시작");
+            sleep(2000);
+            value = new Random().nextInt(10);
+            log("value = " + value);
+            log("Runnable 종료");
+        }
+    }
+}
